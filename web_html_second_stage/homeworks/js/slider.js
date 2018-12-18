0746c1ceac3e343073464b0c0709b2b5939adf15
hchalabyan <hchalabyan@gmail.com>
2018-12-18 16:12:35 +0400

    test_task_2

--- web_html_second_stage/homeworks/js/slider.js
+++ web_html_second_stage/homeworks/js/slider.js
@@ -1,36 +1,65 @@
 var imgArray = ['img/slider/blur-close.jpg','img/slider/codding.jpeg', 'img/slider/code-coding.jpg','img/slider/coffee-flower.jpg','img/slider/coffee_more.jpeg','img/slider/motivation.jpeg','img/slider/pexels.jpeg','img/slider/team_hands.jpeg',];
-
-/* function slider(){
-    var elem = document.getElementsByClassName('slider_container');
-    
-    for(var i = 0; i<imgArray.length; i++){   
-        var imgSrc_one = "url("+imgArray[i]+")"
-        elem[0].style.backgroundImage = imgSrc_one;
-        imgSrc_two = "url("+imgArray[i+1]+")"
-        elem[1].style.backgroundImage = imgSrc_two;
-        imgSrc_three = "url("+imgArray[i+2]+")"
-        elem[2].style.backgroundImage = imgSrc_three;
-    }
-}
-slider(); */
-
 var innerArray = [];
 var gloabalArray=[];
-var elem = document.getElementsByClassName('slider_container');
 var start = 0;
 var step = 3;
 while(start<imgArray.length){
     innerArray = [];
     if(imgArray[start]!=null){
-        var elem = imgArray.splice(start,3);
-        innerArray.push(elem);
+        var innerArray = imgArray.splice(start,3);
         gloabalArray.push(innerArray);
-        elem = [];
     } 
-     
-  
 }  
-console.log(gloabalArray)   
+//console.log(gloabalArray)
+var element = document.getElementsByClassName('slider_container');
+    var imgSrc_one = "url("+gloabalArray[0][0]+")";
+    var imgSrc_two = "url("+gloabalArray[0][1]+")";
+    var imgSrc_three = "url("+gloabalArray[0][2]+")";
+    element[0].style.backgroundImage = imgSrc_one;
+    element[1].style.backgroundImage = imgSrc_two;
+    element[2].style.backgroundImage = imgSrc_three; 
+
+var countPlus = 0;
+var countMinus = 1;
+function right(arr){
+    if(arr[countPlus]!=null){
+        ++countPlus
+        var imgSrc_one = "url("+arr[countPlus][0]+")";
+        var imgSrc_two = "url("+arr[countPlus][1]+")";
+        var imgSrc_three = "url("+arr[countPlus][2]+")";    
+        element[0].style.backgroundImage = imgSrc_one;
+        element[1].style.backgroundImage = imgSrc_two;
+        element[2].style.backgroundImage = imgSrc_three;
+        
+    }
+    else{
+        countPlus = 0;
+    }
+    console.log(arr[countPlus])            
+}
+function left(arr){
+    
+    if(arr[countMinus-1]!=null){
+        var imgSrc_one = "url("+arr[countMinus-1][0]+")";
+        var imgSrc_two = "url("+arr[countMinus-1][1]+")";
+        var imgSrc_three = "url("+arr[countMinus-1][2]+")";    
+        element[0].style.backgroundImage = imgSrc_one;
+        element[1].style.backgroundImage = imgSrc_two;
+        element[2].style.backgroundImage = imgSrc_three;
+    }
+    else{
+        alert('conut nont')
+    }  
+    countMinus--        
+}
+
+document.getElementById("rightSlide").addEventListener("click", function() {
+    right(gloabalArray);
+}, false);
+document.getElementById("leftSlide").addEventListener("click", function() {
+    left(gloabalArray);
+}, false);
+
 
  
 
