d2bf37bd2c7f727a3d68f3e074ecb7847644810c
hchalabyan <hchalabyan@gmail.com>
2018-12-21 11:49:05 +0400

    slider_final

--- web_html_second_stage/homeworks/js/slider.js
+++ web_html_second_stage/homeworks/js/slider.js
@@ -1,66 +1,45 @@
 var imgArray = ['img/slider/blur-close.jpg','img/slider/codding.jpeg', 'img/slider/code-coding.jpg','img/slider/coffee-flower.jpg','img/slider/coffee_more.jpeg','img/slider/motivation.jpeg','img/slider/pexels.jpeg','img/slider/team_hands.jpeg',];
-var innerArray = [];
-var gloabalArray=[];
-var start = 0;
-var step = 3;
-while(start<imgArray.length){
-    innerArray = [];
-    if(imgArray[start]!=null){
-        var innerArray = imgArray.splice(start,3);
-        gloabalArray.push(innerArray);
-    } 
-}  
-console.log(gloabalArray)
-var element = document.getElementsByClassName('slider_container');
-    var imgSrc_one = "url("+gloabalArray[0][0]+")";
-    var imgSrc_two = "url("+gloabalArray[0][1]+")";
-    var imgSrc_three = "url("+gloabalArray[0][2]+")";
-    element[0].style.backgroundImage = imgSrc_one;
-    element[1].style.backgroundImage = imgSrc_two;
-    element[2].style.backgroundImage = imgSrc_three; 
+var step = 6;
+var count = 0;
+var main = document.getElementsByClassName('main')[0];
 
-var countPlus = 1;
+for(var i = 0; i < step; i++){
+    var element = document.createElement('div');
+    element.className = 'slider_container';
+    element.style.backgroundImage = imgArray[i]? "url("+imgArray[i]+")": 'none';
+    element.style.width = 100 / step + '%';
+    main.appendChild(element);
+}
 
 function right(arr){
-    console.log(arr[countPlus]);
-
-            var imgSrc_one = "url("+arr[countPlus][0]+")";
-            var imgSrc_two = "url("+arr[countPlus][1]+")";
-            var imgSrc_three = "url("+arr[countPlus][2]+")";    
-            element[0].style.backgroundImage = imgSrc_one;
-            element[1].style.backgroundImage = imgSrc_two;
-            element[2].style.backgroundImage = imgSrc_three;
-        
-    if(arr[countPlus]!=arr[arr.length-1]){
-        countPlus++ 
+    count += step;    
+    if(count >= arr.length-1){
+        count = 0;
     }
-    else{
-        countPlus = 0;
-    }
-    //console.log(arr[countPlus]) ;         
+    console.log(count)
+    slide(arr);
 }
-var countMinus = gloabalArray.length-1;
+
 function left(arr){
-    console.log(arr[countMinus])
-    var imgSrc_one = "url("+arr[countMinus][0]+")";
-    var imgSrc_two = "url("+arr[countMinus][1]+")";
-    var imgSrc_three = "url("+arr[countMinus][2]+")";    
-    element[0].style.backgroundImage = imgSrc_one;
-    element[1].style.backgroundImage = imgSrc_two;
-    element[2].style.backgroundImage = imgSrc_three;
-    if(arr[0]!=arr[countMinus]){
-       countMinus--
+    count -= step;    
+    if(count < 0){
+        count = ((arr.length % step) == 0) ? arr.length - step : arr.length - ((arr.length) % step);
     }
-    else{
-        countMinus = arr.length-1;
-    }         
+    console.log(count)
+    slide(arr);     
+}
+
+function slide(arr){
+    for(var i = 0; i < step; i++){
+        element[i].style.backgroundImage = arr[count + i]? "url("+arr[count + i]+")": 'none';
+    }
 }
 
 document.getElementById("rightSlide").addEventListener("click", function() {
-    right(gloabalArray);
+    right(imgArray);
 }, false);
 document.getElementById("leftSlide").addEventListener("click", function() {
-    left(gloabalArray);
+    left(imgArray);
 }, false); 
 
 
