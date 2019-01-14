51e2a90f63cb823c0bb1c510622414b16f32e0a6
hchalabyan <hchalabyan@gmail.com>
2019-01-15 01:37:23 +0400

    chips_animation_end

--- web_html_second_stage/chip_anim_2/js/script.js
+++ web_html_second_stage/chip_anim_2/js/script.js
@@ -2,39 +2,36 @@
  
     var elem_group_1 = document.getElementsByClassName("chips_left");
     var elem_group_2 = document.getElementsByClassName("chips_right");
+
     var clearButton = document.getElementById("clear_button");
     var timer = document.getElementById('timer');
     var undoButton = document.getElementById('undo_button');
-    var chipsPart = document.getElementById("chips_part");
+
     var toolsPart = document.getElementById("tools_part");
     var chipsContent = document.getElementById("chips_content");
-    var chipsPartWidth =eval((chipsPart.offsetWidth/window.innerWidth)*100);
-    var toolsPartWidth = toolsPart.offsetWidth;
-    console.log(chipsPartWidth);
-    var chipsContentWidth = chipsContent.offsetWidth;
+    
 
     var topPartWidth = eval((topPart.offsetWidth/window.innerWidth)*100);
-    var topPartBottom = eval(-(window.innerHeight-topPart.offsetHeight)/2+10);
+    var topPartBottom = eval(-(window.innerHeight-topPart.offsetHeight)/2+15);
    
     var arrow = document.getElementById('arrow');
-
-   // var toolsPartBottom = -toolsPart.offsetHeight;
-    var toolsPartBottom = -topPartBottom;
-    var arrowBottom =eval((topPart.offsetHeight/2+ arrow.offsetHeight)-5);
-    console.log(chipsPart.offsetHeight)
-    var status = true;
-    if(status){
+    var toolsPartBottom = eval(-((chipsContent.offsetHeight-toolsPart.offsetHeight)/2+toolsPart.offsetHeight))
+    
+    
+    var arrowBottom = eval(chipsContent.offsetHeight+arrow.offsetHeight);
+    
+    var val = true;
+    count = 0;
         function topToBottom(){
-
+            count++
+            if(val){
             chips_10.style.width = "50%";
-            clearButton.style.left = "-26%";
-            clear_button.style.width = "15%";
-            undo_button.style.width = "15%";
-            undoButton.style.left = "26%";
+            clearButton.style.left = "-25%";
+            clear_button.style.width = "17%";
+            undo_button.style.width = "17%";
+            undoButton.style.left = "25%";
             timer.style.width = "15%";
-           
             toolsPart.style.bottom = toolsPartBottom+"px"; 
-           
             topPart.style.width = topPartWidth*2 + "%";
             topPart.style.bottom = topPartBottom+"px";
             arrow.style.transform = "rotateX(-180deg)";
@@ -47,11 +44,36 @@
                 elem_group_2[i].style.left = "4%";
                 elem_group_2[i].style.width = "9%";
             }  
-        };
-        status = false 
+            val = false;
+           
+        }
+        else {
+            chips_10.style.width = "100%";
+            clearButton.style.left = 0;
+            clear_button.style.width = "90%";
+            undo_button.style.width = "90%";
+            undoButton.style.left = 0;
+            timer.style.width = "35%";
+           
+            toolsPart.style.bottom = 0; 
+           
+            topPart.style.width = topPartWidth+ "%";
+            topPart.style.bottom = 0;
+            arrow.style.transform = "rotateX(360deg)";
+            arrow.style.bottom = 0;
+            for(var i =0; i<elem_group_1.length; i++){
+                elem_group_1[i].style.left = 0;
+                elem_group_1[i].style.width = "15%";
+            }
+            for(var i =0; i<elem_group_2.length; i++){
+                elem_group_2[i].style.left = 0;
+                elem_group_2[i].style.width = "15%";
+            }  
+            val = true; 
+        }  
     }
- 
-topPart.addEventListener("click", function() {
-    topToBottom();
-}, false);
+    
+    topPart.addEventListener("click", function() {
+        topToBottom();
+    },false);
 
