6bdebcf8760e7b41c8faecc10822658d434719ff
hchalabyan <hchalabyan@gmail.com>
2019-01-15 15:42:55 +0400

    toggl_menu

---
+++ web_html_second_stage/homeworks/dom/toggle_menu/js/script_toggle.js
@@ -0,0 +1,23 @@
+var mobileMenu = document.getElementById("mobile_menu");
+
+var cond = true;
+
+var menuItemsList = document.getElementsByClassName("menu_items_list");
+
+function openMenu(){
+    
+    if(cond){
+        
+        menuItemsList[0].style.maxHeight = "140px";
+        
+        cond = false;
+    }
+    else{
+        menuItemsList[0].style.maxHeight = 0;
+        cond = true;
+    }  
+}
+
+mobileMenu.addEventListener('click', function(){
+    openMenu();
+}, false)
