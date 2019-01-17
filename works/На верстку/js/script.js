92f8917fb66cc09a9f1a4df72d6496a38d7269b7
Hripsime Chalabyan <Hripsime.Chalabyan@istc.com>
2019-01-17 19:52:01 +0400

    input-value

--- works/На верстку/js/script.js
+++ works/На верстку/js/script.js
@@ -1,23 +1,12 @@
 function showVal(){
     var val = document.getElementsByTagName("input");
-    var res = document.getElementsByTagName("h5");
-    /* result[0].innerHTML = val[0].value;
-    result[1].innerHTML = val[1].value;
-    result[2].innerHTML = val[2].value;
-    result[3].innerHTML = val[3].value; */
+    var res = document.getElementsByTagName("h6");
    
+    for (var i = 0; i < val.length; i++){
+        console.log(val[i].value);
+        res[i].innerHTML = val[i].value;
+    }
 
-    /* count = 0;
-    while(count<val.lenght){
-        debugger
-        result[count].innerHTML = val[count].value;
-        count = count + 1;
-    } */
-
-    inp_val0.innerHTML = val[0].value;
-    inp_val1.innerHTML = val[1].value;
-    inp_val2.innerHTML = val[2].value;
-    inp_val3.innerHTML = val[3].value; 
 }
 
 sub_button.addEventListener('click', function(){
