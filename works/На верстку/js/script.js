e14547e3cbebd91992bd57d97fc930c814f13229
hchalabyan <hchalabyan@gmail.com>
2019-01-21 01:01:24 +0400

    errors_1

--- works/На верстку/js/script.js
+++ works/На верстку/js/script.js
@@ -2,26 +2,40 @@ function showVal(){
     var val = document.getElementsByTagName("input");
     //var res = document.getElementsByTagName("h6");
    var myData = {};
-    /* for (var i = 0; i < val.length; i++){
-        console.log(val[i].value);
-         res[i].innerHTML = val[i].value; 
-    } */
+   
 
-    myData["userId"] = val[0].value;
-    myData["id"] = val[1].value;
-    myData["title"] = val[2].value;
-    myData["body"] = val[3].value;
-    var myJSdata =  JSON.stringify( myData );
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-           console.log(this.responseText);
+    myData.userId = val[0].value;
+    myData.id = val[1].value;
+    myData.title = val[2].value;
+    myData.body = val[3].value;
+    // var myJSdata =  JSON.stringify( myData );
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //        console.log(this.responseText);
             
+    //     }
+    // };
+    // xhttp.open("POST", 'http://rest.learncode.academy/api/johnbob/friends', true);
+    // xhttp.setRequestHeader("Content-type", "application/json");
+    // xhttp.send(myJSdata);
+
+
+    fetch('http://rest.learncode.academy/api/johnbob/friends', {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: 'POST',
+        body: JSON.stringify(myData)
+    }).then(function(response) {
+        if (val[0].value!=="red") {
+          throw first_name.innerHTML = "not red";
         }
-    };
-    xhttp.open("POST", 'http://rest.learncode.academy/api/johnbob/friends', true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(myJSdata);
+        console.log(response.json());
+        // Read the response as json.
+      })
+      .catch(function(error) {
+        console.log('Looks like there was a problem: \n', error);
+      });
+      
 }
 
 
