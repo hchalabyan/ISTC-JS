4c80815e9ff58dc6a718643abeb3ec3a63137786
hchalabyan <hchalabyan@gmail.com>
2019-01-22 01:27:52 +0400

    mix

--- works/На верстку/js/script.js
+++ works/На верстку/js/script.js
@@ -8,6 +8,24 @@ function showVal(){
     myData.id = val[1].value;
     myData.title = val[2].value;
     myData.body = val[3].value;
+    var email = val[3].value;
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+      
+    
+    if(validateEmail(email)){
+        fetch('http://rest.learncode.academy/api/johnbob/friends', {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: 'POST',
+        body: JSON.stringify(myData)
+        })
+    }
+    else{
+        return not_valid_mail.innerHTML = "not valid email address"
+        //console.log("not valid");
+    }
     // var myJSdata =  JSON.stringify( myData );
     // var xhttp = new XMLHttpRequest();
     // xhttp.onreadystatechange = function() {
@@ -21,20 +39,20 @@ function showVal(){
     // xhttp.send(myJSdata);
 
 
-    fetch('http://rest.learncode.academy/api/johnbob/friends', {
-        headers: { "Content-Type": "application/json; charset=utf-8" },
-        method: 'POST',
-        body: JSON.stringify(myData)
-    }).then(function(response) {
-        if (val[0].value!=="red") {
-          throw first_name.innerHTML = "not red";
-        }
-        console.log(response.json());
-        // Read the response as json.
-      })
-      .catch(function(error) {
-        console.log('Looks like there was a problem: \n', error);
-      });
+    // fetch('http://rest.learncode.academy/api/johnbob/friends', {
+    //     headers: { "Content-Type": "application/json; charset=utf-8" },
+    //     method: 'POST',
+    //     body: JSON.stringify(myData)
+    // }).then(function(response) {
+    //     if (val[0].value!=="red") {
+    //       throw first_name.innerHTML = "not red";
+    //     }
+    //     console.log(response.json());
+    //     // Read the response as json.
+    //   })
+    //   .catch(function(error) {
+    //     console.log('Looks like there was a problem: \n', error);
+    //   });
       
 }
 
