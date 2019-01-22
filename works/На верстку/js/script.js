c8c95738eb33bba9aab0d14d317c14f9ab128a5a
Hripsime Chalabyan <Hripsime.Chalabyan@istc.com>
2019-01-22 19:09:16 +0400

    lll

--- works/На верстку/js/script.js
+++ works/На верстку/js/script.js
@@ -2,15 +2,17 @@ function showVal(){
     var val = document.getElementsByTagName("input");
     //var res = document.getElementsByTagName("h6");
    var myData = {};
+   myData["userId"] = val[0].value;
+    myData["id"] = val[1].value;
+    myData["title"] = val[2].value;
+    myData["body"] = val[3].value;
     /* for (var i = 0; i < val.length; i++){
         console.log(val[i].value);
          res[i].innerHTML = val[i].value; 
     } */
 
-    myData["userId"] = val[0].value;
-    myData["id"] = val[1].value;
-    myData["title"] = val[2].value;
-    myData["body"] = val[3].value;
+
+     
     var myJSdata =  JSON.stringify( myData );
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -21,8 +23,18 @@ function showVal(){
     };
     xhttp.open("POST", 'http://rest.learncode.academy/api/johnbob/friends', true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(myJSdata);
+    xhttp.send(myJSdata); 
+
+
+    /* fetch('http://rest.learncode.academy/api/johnbob/friends', {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: 'POST',
+        body: JSON.stringify({myData})
+    }) */
+
 }
+
+
 
 
 sub_button.addEventListener('click', function(){
