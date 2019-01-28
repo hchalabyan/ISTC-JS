1213aa3b742c2afd78f19509824a91a563c8e61b
hchalabyan <hchalabyan@gmail.com>
2019-01-28 23:41:11 +0400

    end_form

--- advanced_js/homeworks/form_validation/js/script.js
+++ advanced_js/homeworks/form_validation/js/script.js
@@ -6,8 +6,6 @@ function showVal(){
     myData.pass = val[2].value;
     myData.confpass = val[3].value;
 
-    
-
     function validateEmail(email) {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
@@ -72,6 +70,70 @@ passW();
 sub_button.addEventListener('click', function(){
     showVal();
 }, false)
+
+// Login function  ////////////////////////
+
+function login(){
+    var inpData = document.getElementsByClassName("login_inp");
+    var userData = '{"username":"Ripa", "pass":456}'
+
+    var userInfo = JSON.parse(userData);
+
+    if(inpData[0].value.toLowerCase()==userInfo.username.toLowerCase() && inpData[1].value==userInfo.pass){
+        alert('you are welcome')
+    }
+    else{
+        wrong_log.innerHTML = "Wrong username or password"
+    }
+
+    //console.log()
+
+}
+
+
+log_button.addEventListener('click', function(){
+    login();
+}, false)
+
+//End Login function  ////////////////////////
+
+
+
+// // // // // //  Add or remove active class    // // // // // //
+
+
+function activeClass(e) {
+    var elems = document.querySelector(".active");
+   
+    if(elems !==null){
+        elems.classList.remove("active");
+        
+    }
+    e.target.className = "active";
+   
+}
+var sign = document.getElementById("signIn");
+var log = document.getElementById("login");
+function remoteSign(){
+    sign.style.display = "flex";
+    log.style.display = "none";
+}
+function remotLogin(){
+    sign.style.display = "none";
+    log.style.display = "flex";
+}
+remotLogin();
+remoteSign();
+
+
+
+
+// // // // // //End  Add or remove active class    // // // // // //
+
+
+
+
+
 
 
 
